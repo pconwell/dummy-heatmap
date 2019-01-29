6bc9bc9dee1adc32fa37924b28e4f432c4598b42
Patrick Conwell <patrick.conwell@gmail.com>
2019-01-29 17:03:29 -0600

    updates from u/LegalizeAdrenochrome

--- potential-solution.R
+++ potential-solution.R
@@ -17,8 +17,8 @@ plotTheHeat <- function(mat, colors) {
   ys   <- colSums(mat)
   plot.new()
   plot.window(xlim=c(0, ncol(mat)), ylim=c(0, max(ys)*1.5))
-  rect(xs-0.9, 0, xs-0.1, ys, col=cols)
-  text(xs-0.4, ys, round(ys), pos=3, offset=1, srt=90, cex=0.8)
+  rect(xs-0.95, 0, xs-0.05, ys, col=cols, border="white", lwd=0.5)
+  text(xs-0.4, ys, round(ys), pos=3, offset=1, srt=90, cex=0.8, font=2)
   
   screen(screenIDs["right"])
   par(mar=c(0,0,0,0))
@@ -27,8 +27,8 @@ plotTheHeat <- function(mat, colors) {
   ys   <- seq_len(nrow(mat))
   plot.new()
   plot.window(xlim=c(0, max(xs)*1.5), ylim=c(nrow(mat), 0))
-  rect(0, ys-0.9, xs, ys-0.1, col=cols)
-  text(0, ys-0.5, round(xs), pos=4, cex=0.8, col="white")
+  rect(0, ys-0.9, xs, ys-0.1, col=cols, border="white", lwd=0.5)
+  text(0, ys-0.5, round(xs), pos=4, cex=0.8, col="white", font=2)
   
   screen(screenIDs["main"])
   par(mar=c(0,0,0,0))
@@ -37,20 +37,21 @@ plotTheHeat <- function(mat, colors) {
   ys   <- row(mat)
   plot.new()
   plot.window(xlim=c(0, max(xs)), ylim=rev(c(0, max(ys))))
-  rect(xs-1, ys-1, xs, ys, col=cols)
-  text(xs-0.5, ys-0.5, round(mat), cex=0.8, col="white")
+  rect(xs-1, ys-1, xs, ys, col=cols, border="white", lwd=0.5)
+  text(xs-0.5, ys-0.5, round(mat), cex=0.8, col="white", font=2)
   text(xs[1,]-0.5, 0, colnames(mat), pos=3, cex=0.8, xpd=TRUE)
   text(ncol(mat), ys[,1]-0.5, rownames(mat), pos=4, cex=0.8, xpd=TRUE)
   close.screen(screenIDs)
 }
 
 dat <- read.csv("./crashes.csv", as.is=TRUE)
-dat$week <- weekdays(as.Date(dat$collision_date, format="%m/%d/%Y"), abbreviate=FALSE)
+dat$week <- weekdays(as.Date(dat$collision_date, format="%m/%d/%Y"), abbreviate=TRUE)
+dat$week <- factor(dat$week, levels=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
 dat$hour <- format(as.POSIXct(dat$collision_date, format="%m/%d/%Y %H:%M"), "%H")
-dat$week <- factor(dat$week, levels= c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
-dat[order(dat$week), ]
-mat <- tapply(dat$number_injured, list(dat$week, dat$hour), sum)
+mat <- table(dat$week, dat$hour)
 
-png("heat.png", width=1000, height=500)
-plotTheHeat(mat, colorRampPalette(blues9[-1]))
-dev.off()
+
+cols <- colorRampPalette(c("#adddd1", "#3e98af", "#375980"))
+#png("heat.png", width=1000, height=500)
+plotTheHeat(mat, cols)
+#dev.off()
